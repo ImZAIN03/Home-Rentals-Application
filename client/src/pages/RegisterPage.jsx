ca2f3ff2c7a9c02682cf77250a154dadf5cb87a6
Muhammed Zain <muhammedzain2256@gmail.com>
2024-02-11 01:07:18 +0500

    Register Page

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -2,7 +2,42 @@ import React from 'react'
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div className='register'>
+        <div className='register_content'>
+            <form>
+                <input 
+                    placeholder='First Name'
+                    name='firstname'
+                    required
+                />
+                <input 
+                    placeholder='Last Name'
+                    name='lastname'
+                    required
+                />
+                <input 
+                    placeholder='Email'
+                    name='email'
+                    type='email'
+                    required
+                />
+                <input 
+                    placeholder='Password'
+                    name='password'
+                    type='password'
+                    required
+                />
+                <input 
+                    placeholder='Confirm Password'
+                    name='password'
+                    type='password'
+                    required
+                />
+                <input type='file' name='profileImage' accept='image/*' required/>
+            </form>
+            <a href='/login'>Already have an account? Login In here</a>
+        </div>
+    </div>
   )
 }
 
